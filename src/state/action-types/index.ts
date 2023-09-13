9c935fae87a32771e7c119f8fe08ff127baf4867
unstriped <stefanmagnusson90@gmail.com>
2023-09-13 18:11:48 +0200

    add temp content to bundlesReducer

--- src/state/action-types/index.ts
+++ src/state/action-types/index.ts
@@ -1,6 +1,6 @@
 export enum ActionType {
-  MOVE_CELL = "move_cell",
-  DELETE_CELL = "delete_cell",
-  INSERT_CELL_BEFORE = "insert_cell_before",
-  UPDATE_CELL = "update_cell",
+  MOVE_CELL = "cell/move",
+  DELETE_CELL = "cell/delete",
+  INSERT_CELL_BEFORE = "cell/insertBefore",
+  UPDATE_CELL = "cell/update",
 }
